68abd87e19a0fb81b7b278ae68407001dae2570a
noobrs <vinsenyong8763@gmail.com>
2023-06-27 14:29:03 +0800

    Create PutMapping

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -33,6 +33,13 @@ public class StudentController {
         studentService.getStudents(studentId);
     }
 
-
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email
+    ){
+        studentService.updateStudent(studentId, name, email);
+    }
 
 }

3514ce3be6e135a7ceb959cca92b45ddd1ec0afd
noobrs <vinsenyong8763@gmail.com>
2023-06-27 13:24:51 +0800

    Transient 'age'

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student adam = new Student(
+                    "Adam",
+                    "adambrooke@gmail.com",
+                    LocalDate.of(2002, Month.JANUARY, 10)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alexShit@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 10)
+            );
+
+            repository.saveAll(
+                    List.of(adam, alex)
+            );
+        };
+    }
+}
